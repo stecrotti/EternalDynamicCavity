c4539e8a466ea60dd14b28596c4e36209a98befc
stecrotti <sky_96@live.it>
2024-04-08 14:13:14 +0200

    set random seed

--- truncations_vumps_sis.jl
+++ truncations_vumps_sis.jl
@@ -11,6 +11,8 @@ include((@__DIR__)*"/../telegram/notifications.jl")
 using Logging
 Logging.disable_logging(Logging.Info)
 
+Random.seed!(1)
+
 
 function F(λ, ρ)
     SUSCEPTIBLE = 1 
@@ -44,27 +46,24 @@ f = F(λ, ρ)
 # ds = [25, 30]
 # ds = [5, 10, 15, 20, 25]
 # ds = [3, 4, 5, 6]
-ds = 5:5:10
-ds = [16]
+ds = 5:5:15
+# ds = [3, 5]
 
-maxiter = 5
+maxiter = 50
+tol = 0
 
 A0 = reshape([0.210472  0.210472;  0.289528  0.289528], 1,1,2,2)
 
-ε, err, ovl, bel, AA = map(eachindex(ds)) do a
+ε, err, ovl, bel, AA, A = map(eachindex(ds)) do a
     d = ds[a]
-    A, _, εs, errs, ovls, beliefs, As = iterate_bp_vumps(f, d; A0, tol=1e-12, maxiter)
+    A, _, εs, errs, ovls, beliefs, As = iterate_bp_vumps(f, d; A0, tol, maxiter)
     # @telegram "vumps sis $a/$(length(ds)) finished"
-    εs, errs, ovls, beliefs, A
+    εs, errs, ovls, beliefs, A, As
 end |> unzip
 
 using Plots
 
-myimag(x) = isnan(x) ? NaN : imag(x)
-
 pls = map(zip(ε, err, ovl, ds, bel)) do (εs, errs, ovls, d, beliefs)
-    # replace!(εs, 0.0 => NaN)
-    # replace!(ovls, 1.0 => NaN)
     p1 = plot(replace(εs, 0.0 => NaN), xlabel="iter", yaxis=:log10, ylabel="converg error", label="")
     p2 = plot(errs, xlabel="iter", yaxis=:log10, ylabel="trunc err on marginals", label="", title="d=$d")
     p3 = plot(abs.(1 .- replace(ovls, 1.0 => NaN)), xlabel="iter", yaxis=:log10, ylabel="|1-trunc ovl|", label="")
@@ -74,6 +73,6 @@ pls = map(zip(ε, err, ovl, ds, bel)) do (εs, errs, ovls, d, beliefs)
     plot(p1, p2, p3, p4, p5, layout=(1,5), size=(1200,250), margin=5Plots.mm, labelfontsize=9)
 end
 pl = plot(pls..., layout=(length(ds),1), size=(1000, 250*length(ds)), margin=5Plots.mm,
-    xticks = 0:(maxiter÷2):maxiter)
-savefig(pl, (@__DIR__)*"/truncations_vumps_sis.pdf")
+    xticks = 0:(maxiter÷2):maxiter, xlabel="iter")
+savefig(pl, (@__DIR__)*"/truncations_vumps_sis1.pdf")
 @telegram "vumps sis finished"
