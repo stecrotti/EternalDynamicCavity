f2bd58819b52fe372bb24529fa29c971131001eb
stecrotti <sky_96@live.it>
2024-04-12 18:53:56 +0200

    end of the magic week

--- truncations_vumps_glauber.jl
+++ truncations_vumps_glauber.jl
@@ -12,9 +12,6 @@ include((@__DIR__)*"/../telegram/notifications.jl")
 using Logging
 Logging.disable_logging(Logging.Info)
 
-Random.seed!(1)
-
-
 function F(J, β, h)
     function f(xᵢᵗ⁺¹::Integer, xₙᵢᵗ::AbstractVector{<:Integer}, xᵢᵗ::Integer)
         @assert xᵢᵗ⁺¹ ∈ 1:2
@@ -26,9 +23,9 @@ function F(J, β, h)
     end   
 end   
 
-J = 0.6
+J = 0.8
 β = 1.0
-h = 0.2
+h = 0.0
 f = F(J, β, h)
 
 (m_ss,) = equilibrium_observables(RandomRegular(3), J; β, h)
@@ -36,14 +33,16 @@ f = F(J, β, h)
 # ds = [25, 30]
 # ds = [5, 10, 15, 20, 25]
 # ds = [3, 4, 5, 6]
-ds = 3:6
+ds = 2:2:10
 # ds = [3, 5]
 
-maxiter = 50
-tol = 1e-12
+maxiter = 30
+tol = 1e-15
 
 # A0 = reshape([0.31033984998979236 0.31033984998979214; 0.18966015001020783 0.1896601500102077], 1,1,2,2)
+# Random.seed!(3)
 A0 = rand(1,1,2,2)
+A0 = reshape([0.4 0.4; 0.2 0.2], 1, 1, 2, 2)
 
 ε, err, ovl, bel, AA, A = map(eachindex(ds)) do a
     d = ds[a]
@@ -58,17 +57,19 @@ pls = map(zip(ε, err, ovl, ds, bel)) do (εs, errs, ovls, d, beliefs)
     p1 = plot(replace(εs, 0.0 => NaN), xlabel="iter", yaxis=:log10, ylabel="converg error", label="")
     p2 = plot(errs, xlabel="iter", yaxis=:log10, ylabel="trunc err on marginals", label="", title="d=$d")
     p3 = plot(abs.(1 .- replace(ovls, 1.0 => NaN)), xlabel="iter", yaxis=:log10, ylabel="|1-trunc ovl|", label="")
-    p4 = plot([reduce(-, b) for b in beliefs], ylabel="Re[single-var magnetiz]", ylims=(-2,2), label="")
+    p4 = plot([reduce(-, b) for b in beliefs], ylabel="single-var magnetiz", ylims=(-2,2), label="")
     hline!(p4, [m_ss] , label="", ls=:dash)
-    p5 = plot([minimum(b) for b in beliefs], ylabel="min marginal", label="")
-    plot(p1, p2, p3, p4, p5, layout=(1,5), size=(1200,250), margin=5Plots.mm, labelfontsize=9)
+    # p5 = plot([minimum(b) for b in beliefs], ylabel="min marginal", label="")
+    plot(p1, p2, p3, p4, layout=(1,4), size=(1200,250), margin=5Plots.mm, labelfontsize=9)
 end
 pl = plot(pls..., layout=(length(ds),1), size=(1000, 250*length(ds)), margin=5Plots.mm,
     xticks = 0:(maxiter÷2):maxiter, xlabel="iter")
-savefig(pl, (@__DIR__)*"/truncations_vumps_glauber2.pdf")
-@telegram "vumps glauber finished"
+savefig(pl, (@__DIR__)*"/vumps_glauber.pdf")
 
 ps = [reduce(-,b[findlast(x->!all(isnan, x), b)]) for b in bel]
 pl_ps = scatter(ds, ps, xlabel="bond dim", ylabel="magnetiz", label="")
-hline!(pl_ps, [m_ss], label="steady-state", ylims=(-1,1))
-savefig(pl_ps, "truncations_vumps_glauber_ps.pdf")
+hline!(pl_ps, [m_ss], ylims=(0.9,1.), label="true steady-state")
+plot!(pl_ps, title="Glauber J=$J, h=$h, β=$β")
+savefig(pl_ps, "vumps_glauber_bonddims.pdf")
+
+@telegram "vumps glauber finished"
