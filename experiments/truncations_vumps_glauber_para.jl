bb1c250ba7c98a84f699ac59625ef55240d9c0a8
stecrotti <sky_96@live.it>
2024-07-15 13:38:57 +0200

    use MPBP syntax for glauber bp+vumps

--- experiments/truncations_vumps_glauber_para.jl
+++ experiments/truncations_vumps_glauber_para.jl
@@ -7,6 +7,7 @@ using LinearAlgebra
 using ProgressMeter
 using MatrixProductBP, MatrixProductBP.Models
 using JLD2
+include("../src/mpbp.jl")
 
 include((@__DIR__)*"/../../telegram/notifications.jl")
 
@@ -16,49 +17,41 @@ Logging.disable_logging(Logging.Info)
 J = 0.4
 β = 1.0
 h = 0.2
-f = HomogeneousGlauberFactor(J, h, β)
-
-(m_ss,) = equilibrium_observables(RandomRegular(3), J; β, h)
-
-ds = 2:2:14
+k = 3
 
 maxiter = 50
 tol = 1e-15
+
 maxiter_vumps = 100
+tol_vumps = 1e-14
 
-# Random.seed!(3)
-A0 = rand(1,1,2,2)
-A0 = reshape([10 10; 0.2 0.2], 1, 1, 2, 2)
-A_current = copy(A0)
+d = 15
+svd_trunc = TruncVUMPS(d; maxiter=maxiter_vumps, tol=tol_vumps)
+maxiter = 20
+tol = 0
 
-ε, err, ovl, bel, AA, A = map(eachindex(ds)) do a
-    d = ds[a]
-    A0new = A_current #.+ 1e-3*randn.()
-    global A_current, _, εs, errs, ovls, beliefs, As = iterate_bp_vumps_mpskit(f, d; A0=A0new, tol, maxiter, maxiter_vumps)
-    εs, errs, ovls, beliefs, copy(A_current), As
-end |> unzip
+spin(x,args...) = 3-2x
+bp = mpbp_stationary_infinite_graph(k, [HomogeneousGlauberFactor(J, h, β)], 2)
+
+function MyCB(bp)
+    c = zeros(ComplexF64, 0)
+    cb = CB_BP(bp; f=spin)
+    function callback(bp, it, svd_trunc)
+        push!(c, only(only(autocorrelations(spin, bp))))
+        cb(bp, it, svd_trunc)
+    end
+end
+
+cb = MyCB(bp)
+iterate!(bp; maxiter, svd_trunc, tol, cb)
+
+(m_ss, r_ss) = equilibrium_observables(RandomRegular(3), J; β, h)
 
 using Plots
+pl1 = plot([only(only(m)) for m in cb.cb.m], xlabel="iter", ylabel="magnetiz")
+hline!(pl1, [m_ss, -m_ss], ls=:dash, ylims=(-1,1))
 
-pls = map(zip(ε, err, ovl, ds, bel)) do (εs, errs, ovls, d, beliefs)
-    p1 = plot(replace(εs, 0.0 => NaN), xlabel="iter", yaxis=:log10, ylabel="converg error", label="")
-    p2 = plot(errs, xlabel="iter", yaxis=:log10, ylabel="trunc err on marginals", label="", title="d=$d")
-    p3 = plot(abs.(1 .- replace(ovls, 1.0 => NaN)), xlabel="iter", yaxis=:log10, ylabel="|1-trunc ovl|", label="")
-    p4 = plot([reduce(-, b) for b in beliefs], ylabel="single-var magnetiz", ylims=(-1.1,1.1), label="")
-    hline!(p4, [m_ss] , label="", ls=:dash)
-    plot(p1, p2, p3, p4, layout=(1,4), size=(1200,250), margin=5Plots.mm, labelfontsize=9)
-end
-pl = plot(pls..., layout=(length(ds),1), size=(1000, 250*length(ds)), margin=5Plots.mm,
-    xticks = 0:(maxiter÷2):maxiter, xlabel="iter")
-savefig(pl, (@__DIR__)*"/vumps_glauber_para.pdf")
+pl2 = plot(real(cb.c), xlabel="iter", ylabel="2-time autocorr")
+hline!(pl2, [r_ss], ls=:dash, ylims=(-1,1))
 
-ps = [reduce(-,b[findlast(x->!all(isnan, x), b)]) for b in bel]
-pl_ps = scatter(ds, ps, xlabel="bond dim", ylabel="magnetiz", label="",
-    ms=3, c=:black, xticks=ds)
-hline!(pl_ps, [m_ss], label="true steady-state")
-plot!(pl_ps, title="Glauber J=$J, h=$h, β=$β")
-savefig(pl_ps, "vumps_glauber_bonddims_para.pdf")
-
-jldsave((@__DIR__)*"/../data/vumps_glauber_para.jld2"; J, h, β, ds, A0, ε, err, ovl, bel, AA, A, maxiter, ps, m_ss)
-
-@telegram "vumps glauber para finished"
+plot(pl1, pl2)
