949df294204b4522dd24cc9c4ab20ffca69958ce
stecrotti <sky_96@live.it>
2024-10-28 00:24:10 +0100

    sis continuous

---
+++ scripts/sis_continuous/sis_gillespie.jl
@@ -0,0 +1,60 @@
+using Graphs, IndexedGraphs, Statistics
+using MatrixProductBP, MatrixProductBP.Models
+using JLD2
+
+include("../../../telegram/notifications.jl");
+
+T = 600        # final time
+k = 3         # degree
+λs = [0.1, 0.2, 0.3, 0.4]      # rate of transmission
+ρ = 0.1       # rate of recovery
+γ = 0.9      # prob. of zero patient
+# N = 10^4
+nsamples = 5*10^3
+
+Ns = [100, 500, 2000]
+
+seed = 0
+
+ps_gillespie = map(λs) do λ
+    ps = map(Ns) do N
+        gg = random_regular_graph(N, k; seed)
+        g = IndexedGraph(gg)
+
+        sis = SIS(g, λ, ρ, T; γ)
+        p_gill,_ = continuous_sis_sampler(sis, T, λ, ρ; nsamples, sites=1:N,
+            Δt=1e-1, discard_dead_epidemics=false)
+        m_gill = mean(p_gill)
+
+        println("\nFinished N=$N\n")
+
+        Tmean = T ÷ 4
+        p_gillespie = mean(m_gill[end-Tmean:end])
+    end
+    println("\nFinished λ=$λ\n")
+    ps
+end
+
+λ = 0.4
+ps = map(Ns) do N
+    gg = random_regular_graph(N, k; seed)
+    g = IndexedGraph(gg)
+
+    sis = SIS(g, λ, ρ, T; γ)
+    p_gill,_ = continuous_sis_sampler(sis, T, λ, ρ; nsamples, sites=1:N,
+        Δt=1e-1, discard_dead_epidemics=false)
+    m_gill = mean(p_gill)
+
+    println("\nFinished N=$N\n")
+
+    Tmean = T ÷ 4
+    p_gillespie = mean(m_gill[end-Tmean:end])
+end
+
+ps_gillespie_cp = deepcopy(ps_gillespie)
+push!(ps_gillespie, ps)
+
+
+jldsave((@__DIR__)*"/../../data/sis_gillespie.jld2"; k, λs, ρ, ps_gillespie, Ns, nsamples)
+
+@telegram "SIS gillespie"
