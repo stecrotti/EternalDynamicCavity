788c7c57125918ade0b95a7515dbd3ad3f64e874
stecrotti <sky_96@live.it>
2024-10-28 00:22:16 +0100

    move code out of MPSExperiments module

--- src/mpbp.jl
+++ src/mpbp.jl
@@ -1,7 +1,10 @@
-using TensorTrains: TensorTrains, SVDTrunc, summary_compact
-using TensorTrains.UniformTensorTrains: InfiniteUniformTensorTrain, dot
+using TensorTrains: TensorTrains, SVDTrunc, summary_compact, _reshape1
+using TensorTrains.UniformTensorTrains: InfiniteUniformTensorTrain, dot, rand_infinite_uniform_tt
 using MatrixProductBP: MatrixProductBP, InfiniteUniformMPEM2
 using ProgressMeter: ProgressUnknown, next!
+using MPSKit
+using TensorKit
+using TensorCast
 
 struct TruncVUMPS{TI<:Integer, TF<:Real} <: SVDTrunc
     d       :: TI
@@ -12,14 +15,47 @@ TruncVUMPS(d::Integer; maxiter=100, tol=1e-14) = TruncVUMPS(d, maxiter, tol)
 
 TensorTrains.summary(svd_trunc::TruncVUMPS) = "VUMPS truncation to bond size m'="*string(svd_trunc.d)
 
+function truncate_vumps(A::Array{F,3}, d; 
+        init = rand(d, size(A,2), d),
+        maxiter = 100, kw_vumps...) where {F}
+    ψ = InfiniteMPS([TensorMap(init, (ℝ^d ⊗ ℝ^size(A,2)), ℝ^d)])
+    Q = size(A, 2)
+    m = size(A, 1)
+    @assert size(A, 3) == m
+    t = TensorMap(A,(ℝ^m ⊗ ℝ^Q), ℝ^m) # the same but as a type digestible by MPSKit.jl
+    ψ₀ = InfiniteMPS([t])
+    II = DenseMPO([MPSKit.add_util_leg(id(storagetype(MPSKit.site_type(ψ₀)), physicalspace(ψ₀, i)))
+        for i in 1:length(ψ₀)])
+    alg = VUMPS(; maxiter, kw_vumps...) # variational approximation algorithm
+    # alg = IDMRG1(; maxiter)
+    @assert typeof(ψ) == typeof(ψ₀)
+    ψ_, = approximate(ψ, (II, ψ₀), alg)   # do the truncation
+    @assert typeof(ψ) == typeof(ψ_)
+
+    ovl = abs(dot(ψ_, ψ₀))
+    B = reshape(only(ψ_.AL).data, d, Q, d)
+    return B, ovl, ψ_
+end
 
 function TensorTrains.compress!(A::InfiniteUniformTensorTrain; svd_trunc::TruncVUMPS=TruncVUMPS(4),
-        is_orthogonal::Symbol=:none, init = rand(svd_trunc.d, prod(size(A.tensor)[3:end]), svd_trunc.d))
+        is_orthogonal::Symbol=:none, init = rand_infinite_uniform_tt(svd_trunc.d, size(A.tensor)[3:end]...))
     (; d, maxiter, tol) = svd_trunc
     qs = size(A.tensor)[3:end]
     B = reshape(A.tensor, size(A.tensor)[1:2]..., prod(qs))
     Bperm = permutedims(B, (1,3,2))
-    Btruncperm, = truncate_vumps(Bperm, d; maxiter, tol, init)
+    # reduce or expand `init` to match bond dimension `svd_trunc.d`
+    s = size(init.tensor)
+    init_resized = if s[1] != svd_trunc.d
+        init_ = InfiniteUniformTensorTrain(zeros(svd_trunc.d, svd_trunc.d, size(A.tensor)[3:end]...))
+        init_.tensor[1:s[1],1:s[2],fill(:,length(qs))...] = init.tensor
+        init_
+    else
+        init
+    end
+    if size(permutedims(_reshape1(init_resized.tensor), (1,3,2))) != size(rand(svd_trunc.d, prod(size(A.tensor)[3:end]), svd_trunc.d))
+        @show size(permutedims(_reshape1(init_resized.tensor), (1,3,2))) size(rand(svd_trunc.d, prod(size(A.tensor)[3:end]), svd_trunc.d))
+    end
+    Btruncperm, = truncate_vumps(Bperm, d; maxiter, tol, init = permutedims(_reshape1(init_resized.tensor), (1,3,2)))
     Btrunc = permutedims(Btruncperm, (1,3,2))
     A.tensor = reshape(Btrunc, size(Btrunc)[1:2]..., qs...)
     return A
