5daa88481f35ebb33497e5bc916e9e83a5158f25
stecrotti <sky_96@live.it>
2024-11-13 18:26:05 +0100

    update gillespie

--- scripts/sis_continuous/sis_gillespie.jl
+++ scripts/sis_continuous/sis_gillespie.jl
@@ -35,26 +35,6 @@ ps_gillespie = map(λs) do λ
     ps
 end
 
-λ = 0.4
-ps = map(Ns) do N
-    gg = random_regular_graph(N, k; seed)
-    g = IndexedGraph(gg)
-
-    sis = SIS(g, λ, ρ, T; γ)
-    p_gill,_ = continuous_sis_sampler(sis, T, λ, ρ; nsamples, sites=1:N,
-        Δt=1e-1, discard_dead_epidemics=false)
-    m_gill = mean(p_gill)
-
-    println("\nFinished N=$N\n")
-
-    Tmean = T ÷ 4
-    p_gillespie = mean(m_gill[end-Tmean:end])
-end
-
-ps_gillespie_cp = deepcopy(ps_gillespie)
-push!(ps_gillespie, ps)
-
-
 jldsave((@__DIR__)*"/../../data/sis_gillespie.jld2"; k, λs, ρ, ps_gillespie, Ns, nsamples)
 
 @telegram "SIS gillespie"
