1ddc65f2de299739f5e8ae2814b1e43944536fa5
stecrotti <sky_96@live.it>
2024-12-03 12:44:40 +0100

    supress warnings from setup file

--- setup.jl
+++ setup.jl
@@ -1,6 +1,11 @@
 import Pkg
+import Logging
 
+# activate necessary source code
 Pkg.activate(@__DIR__)
-
 Pkg.add(url="https://github.com/stecrotti/MPSKit.jl")
 # Pkg.add(url="https://github.com/stecrotti/MatrixProductBP.jl", rev="uniform_tt")
+
+# disable long VUMPS outputs
+Logging.disable_logging(Logging.Info)
+Logging.disable_logging(Logging.Warn)
