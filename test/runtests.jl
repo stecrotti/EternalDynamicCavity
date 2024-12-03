0166783972df80b4150c4c65fe34befacf2329bc
stecrotti <sky_96@live.it>
2024-12-03 13:10:26 +0100

    re-organize test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,8 @@
 using MPSExperiments
 using Test
+using MatrixProductBP, MatrixProductBP.Models
 
-using TensorTrains, Random, ProgressMeter
-using TensorTrains.UniformTensorTrains
-import TensorTrains.UniformTensorTrains: transfer_operator, leading_eig
-
-using ForwardDiff: gradient
-using FiniteDifferences
-
-@testset "Uniform Tensor Train - basics" begin
-    include("uniform_tt.jl")
+@testset "Glauber" begin
+    include("glauber_ss.jl")
 end
 
-@testset "Uniform Tensor Train - derivatives" begin
-    include("uniform_tt_derivatives.jl")
-end
