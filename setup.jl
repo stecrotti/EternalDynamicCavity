e294dac31f3be035997ffad56491a4b88acb0a27
stecrotti <sky_96@live.it>
2024-12-05 16:05:23 +0100

    use main of MatrixProductBP

--- setup.jl
+++ setup.jl
@@ -4,7 +4,6 @@ import Logging
 # activate necessary source code
 Pkg.activate(@__DIR__)
 Pkg.add(url="https://github.com/stecrotti/MPSKit.jl")
-# Pkg.add(url="https://github.com/stecrotti/MatrixProductBP.jl", rev="uniform_tt")
 
 # disable long VUMPS outputs
 Logging.disable_logging(Logging.Info)
