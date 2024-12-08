9cb9219ef956a53e48c1c26b13832cdcaf840954
stecrotti <sky_96@live.it>
2024-12-09 00:11:06 +0100

    use official version of MPSKit

--- setup.jl
+++ setup.jl
@@ -3,7 +3,6 @@ import Logging
 
 # activate necessary source code
 Pkg.activate(@__DIR__)
-Pkg.add(url="https://github.com/stecrotti/MPSKit.jl")
 
 # disable long VUMPS outputs
 Logging.disable_logging(Logging.Info)
